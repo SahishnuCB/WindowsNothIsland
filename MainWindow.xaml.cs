96426766c103ad55e5d24ae91f4edc9d341a49cb
SahishnuCB <sahishnucbh@gmail.com>
2026-04-26 12:31:41 +0530

    Initial WPF notch island prototype

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace WindowsNothIsland
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += (_, _) =>
+            {
+                Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+                Top = 0;
+            };
+        }
+
+        private void Island_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            AnimateIsland(560, 170, 18);
+        }
+
+        private void Island_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            AnimateIsland(250, 48, 18);
+        }
+
+        private void AnimateIsland(double newWidth, double newHeight, double bottomRadius)
+        {
+            var duration = TimeSpan.FromMilliseconds(380);
+
+            var widthAnim = new DoubleAnimation
+            {
+                To = newWidth,
+                Duration = duration,
+                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+            };
+
+            var heightAnim = new DoubleAnimation
+            {
+                To = newHeight,
+                Duration = duration,
+                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+            };
+
+            var radiusAnim = new CornerRadiusAnimation
+            {
+                To = new CornerRadius(0, 0, bottomRadius, bottomRadius),
+                Duration = duration
+            };
+
+            Island.BeginAnimation(WidthProperty, widthAnim);
+            Island.BeginAnimation(HeightProperty, heightAnim);
+            Island.BeginAnimation(Border.CornerRadiusProperty, radiusAnim);
+        }
+    }
+
+    public class CornerRadiusAnimation : AnimationTimeline
+    {
+        public CornerRadius? From { get; set; }
+        public CornerRadius? To { get; set; }
+
+        public override Type TargetPropertyType => typeof(CornerRadius);
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new CornerRadiusAnimation();
+        }
+
+        public override object GetCurrentValue(
+            object defaultOriginValue,
+            object defaultDestinationValue,
+            AnimationClock animationClock)
+        {
+            var from = From ?? (CornerRadius)defaultOriginValue;
+            var to = To ?? (CornerRadius)defaultDestinationValue;
+
+            if (animationClock.CurrentProgress == null)
+                return from;
+
+            double progress = animationClock.CurrentProgress.Value;
+
+            return new CornerRadius(
+                from.TopLeft + (to.TopLeft - from.TopLeft) * progress,
+                from.TopRight + (to.TopRight - from.TopRight) * progress,
+                from.BottomRight + (to.BottomRight - from.BottomRight) * progress,
+                from.BottomLeft + (to.BottomLeft - from.BottomLeft) * progress
+            );
+        }
+    }
+}
